cef2b9a50f41bba39af71590b24926e3713bf0f4
hans <brandnewidhans2.gmail.com>
2023-09-13 15:11:36 +0900

    week23 v07

--- week23/src/PokemonGame.java
+++ week23/src/PokemonGame.java
@@ -2,21 +2,45 @@
 
 public class PokemonGame {
     public static void main(String[] args) {
-        Pokemon pikachu = new Pokemon();
+        Pokemon squirtle = new Pokemon("꼬부기", 1);
+        Pokemon pikachu = new Pokemon("피카츄");
         Pokemon charizard = new Pokemon();
 
-        pikachu.name = "피카츄";
-        charizard.name = "리자몽";
+        System.out.println(pikachu.getName()
+                + "의 체력은 " + pikachu.getHp()
+                + "이고 레벨은 " + pikachu.getLevel()
+                + "입니다");
 
-        pikachu.level = 3;
-        pikachu.hp = 100;
+        squirtle.evolve(squirtle);
+        System.out.println(squirtle.getName());
+        squirtle.evolve(squirtle);
 
-        charizard.level = 36;
-        charizard.hp = 800;
+//        pikachu.name = "피카츄";
+//        charizard.name = "리자몽";
 
-        System.out.println(charizard.name + "의 체력은 " + charizard.hp + "이고 레벨은 " + charizard.level + "입니다");
+        charizard.setName("리자몽");
+
+//        pikachu.setLevel(23);
+//        pikachu.setHp(500);
+        pikachu.setLevel(23).setHp(500);  // method chaining.
+
+
+        charizard.setLevel(36);
+
+        System.out.println(charizard.getName() + "의 체력은 " + charizard.getHp() + "이고 레벨은 " + charizard.getLevel() + "입니다");
         charizard.evolve(charizard);
         pikachu.evolve(pikachu);
         pikachu.evolve(pikachu);
+        pikachu.setName("라이츄");
+        System.out.println(pikachu.getName());
+        pikachu.evolve(pikachu);
+
+        System.out.println(pikachu.getName()
+                + "의 체력은 " + pikachu.getHp()
+                + "이고 레벨은 " + pikachu.getLevel()
+                + "입니다");
+
+        charizard.attack(squirtle);
+        squirtle.attack();
     }
 }
