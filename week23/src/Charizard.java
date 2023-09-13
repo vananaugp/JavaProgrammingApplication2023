ec4b783b548d6093255652d46b94381da60a946c
hans <brandnewidhans2.gmail.com>
2023-09-13 15:22:13 +0900

    week23 v13

--- week23/src/Charizard.java
+++ week23/src/Charizard.java
@@ -1,5 +1,4 @@
 
-
 public class Charizard extends Pokemon{
     public Charizard() {
         System.out.println("자식 클래스(리자몽)의 기본 생성자");
@@ -19,5 +18,9 @@ public class Charizard extends Pokemon{
     public void attack() {
         //super.attack();
         System.out.println(this.name + "이(가) 광역 화염 공격을 시전합니다");
+    }
+
+    public void type(){
+        System.out.println(this.name + " 화염 속성!!");
     }
 }
