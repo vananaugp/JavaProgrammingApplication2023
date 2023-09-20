e8ddd0eee7bb1863478c3b77617476b0ba585338
hans <brandnewidhans2.gmail.com>
2023-09-20 14:03:35 +0900

    week24 v16

---
+++ week24/src/Flyable.java
@@ -0,0 +1,14 @@
+
+
+public interface Flyable {
+//    public Flyable(){
+//
+//    }
+
+    //Pokemon pokemon;
+
+    //void fly();
+    default void fly(){
+        System.out.println("날아 갑니다!");
+    }
+}
