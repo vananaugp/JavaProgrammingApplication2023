ee95062ef442ab2c226bbb55c5311b2044ee1017
hans <brandnewidhans2.gmail.com>
2023-10-18 14:12:07 +0900

    week28 v40" ; :wq qsdsddw ds dds..21 e xa

---
+++ week28/src/generic/LisdDemo4.java
@@ -0,0 +1,16 @@
+package generic;
+
+import Pokemonster.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LisdDemo4 {
+    public static void main(String[] args) {
+        List<Pokemon> pokemons = new ArrayList<>(Arrays.asList(new Pikachu(new NoFly()), new Charizard(new Wings())));
+
+        for (Pokemon pokemon : pokemons)
+            pokemon.performFly();
+    }
+}
